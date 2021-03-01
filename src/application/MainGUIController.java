7991f52a1c4ac2d8463040ac3e036a28bf573c11
waeekron <walter.e.kronqvist@student.jyu.fi>
2021-03-01 21:27:24 +0200

    HT vaihe 3

---
+++ src/application/MainGUIController.java
@@ -0,0 +1,52 @@
+package application;
+
+import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ModalController;
+import javafx.fxml.FXML;
+
+
+public class MainGUIController {
+	
+	
+	@FXML private void handleKirjauduUlos() {
+		Dialogs.showMessageDialog("Toimintoa Kirjaudu ulos ei vielä ole");
+	}
+	
+	
+	@FXML private void handleLopeta() {
+		ModalController.showModal(MainGUIController.class.getResource("quitConfirmationView.fxml"), "Lopeta", null,"");
+	}
+	
+	
+	@FXML private void handleApua() {
+		ModalController.showModal(MainGUIController.class.getResource("infoDialog.fxml"), "Käyttöohje", null,"");
+	}
+	
+	
+	@FXML private void handleTietoja() {
+		Dialogs.showMessageDialog("Toimintoa Tietoa... ei vielä ole");
+	}
+	
+	
+	@FXML private void handleLisaaMerkinta() {
+		ModalController.showModal(MainGUIController.class.getResource("addDreamView.fxml"), "Merkinnän hallinta", null,"");
+	}
+	
+	
+	
+	@FXML private void handleJarjesta() {
+		Dialogs.showMessageDialog("Toimintoa Järjesta ei vielä ole");
+	}
+	
+	
+	@FXML private void handleHae() {
+		Dialogs.showMessageDialog("Toimintoa Hae ei vielä ole");
+	}
+	
+	
+	@FXML private void handlePoista() {
+		ModalController.showModal(MainGUIController.class.getResource("deleteView.fxml"), "Poistetaanko", null,"");
+	}
+	
+	
+}
